68feab74d6aed217de7f6caafd2eab181db82644
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 10:06:12 -0500

    Noncompliant Solution

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,3 +1,25 @@
+
+//Jonathon Irigoyen
+//MET01 vulnerability solution
+
+public class R06_MET01_J {
+
+  public static void main(String[] args) {
+  int a = 17;
+  int b = 42;
+  int c = getAbsAdd(a,b);
+  System.out.println("c = " + c);
+
+  a = Integer.MIN_VALUE; //Testing the exception
+  b = Integer.MAX_VALUE;
+  c = getAbsAdd(a,b);
+  System.out.println("c = " + c);
+
+}
+
+
+//https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
+
 public static int getAbsAdd(int x, int y) {
   assert x != Integer.MIN_VALUE;
   assert y != Integer.MIN_VALUE;
@@ -6,4 +28,5 @@ public static int getAbsAdd(int x, int y) {
   assert (absX <= Integer.MAX_VALUE - absY);
   return absX + absY;
 }
-//Usage: getAbsAdd(Integer.MIN_VALUE, 1);
+
+}
