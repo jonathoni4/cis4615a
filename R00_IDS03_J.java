39aa8339d996f535c679f967a166d99a860b8f32
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 11:25:15 -0500

    Noncompliant Solution

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,5 +1,19 @@
-if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
-} else {
-  logger.severe("User login failed for: " + username);
+//Jonathon Irigoyen
+//Input Validation and Data Sanitization
+// https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
+import java.util.logging.*;
+
+public class R00_IDS03_J {
+
+  public static void main(String[] args){
+
+    boolean loginSuccessful = true;
+    String username = "Jonathon";
+    Logger logger = Logger.getLogger(R00_IDS03_J.class.getName()); 
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + username);
+    } else {
+      logger.severe("User login failed for: " + username);
+  }
+}
 }
