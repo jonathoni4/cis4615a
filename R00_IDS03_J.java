bb944341547506ca11137f5e2461a4a427f7109f
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 11:32:48 -0500

    Compliant Solution

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,8 +1,25 @@
-if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + username);
-} else {
-  logger.severe("User login failed for: " + username);
+//Jonathon Irigoyen
+//Input Validation and Data Sanitization
+// https://wiki.sei.cmu.edu/confluence/display/java/IDS03-J.+Do+not+log+unsanitized+user+input
+import java.util.logging.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class R00_IDS03_J {
+
+  public static void main(String[] args){
+
+    boolean loginSuccessful = false;
+    String username = "Jonathon@";
+    Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+    if (loginSuccessful) {
+      logger.severe("User login succeeded for: " + sanitizeUser(username));
+    } else {
+      logger.severe("User login failed for: " + sanitizeUser(username));
+    }
 }
-public String sanitizeUser(String username) {
-  return Pattern.matches("}A-Za-z0-9.|+", username) ? username : "unauthorized user";
+
+public static String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
+}
 }
