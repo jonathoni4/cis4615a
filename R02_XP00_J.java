36b56791bf0fec97c55f7e76cd4b5b606494e62a
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 10:31:21 -0500

    Compliant Solution

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,7 +1,23 @@
-public void deleteFile(){
+
+import java.io.File;
+//Jonathon Irigoyen
+//Expressions
+public class R02_XP00_J {
+
+  public static void main(String[] args) {
+
+    deleteFile();
+
+}
+//https://wiki.sei.cmu.edu/confluence/display/java/EXP00-J.+Do+not+ignore+values+returned+by+methods
+public static void deleteFile(){
+
   File someFile = new File("someFileName.txt");
   // Do something with someFile
   if (!someFile.delete()) {
-    //Hanlde failure to delete the file
+    // Handle failure to delete the file
+     throw new RuntimeException("Did not delete file.");
   }
+
+}
 }
