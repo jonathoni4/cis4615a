f9f71c34863bd09c9604bf97fce93c6aa597574a
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 10:17:54 -0500

    Noncompliant Solution

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,5 +1,18 @@
-public void deleteFile(){
+
+import java.io.File;
+//Jonathon Irigoyen
+//Expressions
+public class R02_XP00_J {
+
+  public static void main(String[] args) {
+
+    deleteFile();
+
+}
+//https://wiki.sei.cmu.edu/confluence/display/java/EXP00-J.+Do+not+ignore+values+returned+by+methods
+public static void deleteFile(){
   File someFile = new File("someFileName.txt");
   // Do something with someFile
   someFile.delete();
+}
 }
