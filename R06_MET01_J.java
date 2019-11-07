14e50c8af0ed63e418f836c5db0ec10842bd11b3
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 10:02:31 -0500

    Compliant Solution

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,3 +1,25 @@
+
+//Jonathon Irigoyen
+//MET01 vulnerability solution
+
+public class R06_MET01_J {
+
+  public static void main(String[] args) {
+  int a = 17;
+  int b = 42;
+  int c = getAbsAdd(a,b);
+  System.out.println("c = " + c);
+
+  a = Integer.MIN_VALUE; //Testing the exception
+  b = Integer.MAX_VALUE;
+  c = getAbsAdd(a,b);
+  System.out.println("c = " + c);
+
+}
+
+
+//https://wiki.sei.cmu.edu/confluence/display/java/MET01-J.+Never+use+assertions+to+validate+method+arguments
+
 public static int getAbsAdd(int x, int y) {
   if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
     throw new IllegalArgumentException();
@@ -9,4 +31,5 @@ public static int getAbsAdd(int x, int y) {
   }
   return absX + absY;
 }
-//Usage: getAbsAdd(Integer.MIN_VALUE, 1);
+
+}
