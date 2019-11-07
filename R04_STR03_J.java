b02f9853beb02b6971288965bcf22c84a9298744
Jonathon Irigoyen <Jonathon@Joes-MacBook-Air.local>
2019-11-07 10:47:43 -0500

    Compliant Solution

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,5 +1,27 @@
-BigInteger x = new BigInteger("530500452766");
-String s = x.toString(); //Valid character data
-byte[] byteArray = s.getBytes();
-String ns = new String(ByteArray);
-x = new BigInteger(ns)
+//Jonathon Irigoyen
+//Characters and Strings
+// https://wiki.sei.cmu.edu/confluence/display/java/STR03-J.+Do+not+encode+noncharacter+data+as+a+string
+
+import java.math.BigInteger;
+
+public class R04_STR03_J {
+
+  public static void main(String[] args) {
+
+    BigInteger x = new BigInteger("530500452766");
+    BigInteger original = new BigInteger("530500452766");
+    String s = x.toString(); //Valid character data
+    byte[] byteArray = s.getBytes();
+    String ns = new String(byteArray);
+    x = new BigInteger(ns);
+    
+    if(ns.equals("530500452766")){
+      System.out.println("Correct BigInteger.");
+    }
+    else {
+      System.out.println("Incorrect BigInteger");
+    }
+
+}
+
+}
