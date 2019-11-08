6be3be0fa040cf42785991400a8b4d2ec7485caa
Jonathon Irigoyen <Jonathon@joes-air.home>
2019-11-07 19:28:16 -0500

    Compliant Solution

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,20 @@
-public static int getInteger(DataInputStream is) throws IOException {
-  return is.readInt();
+
+
+//Jonathon Irigoyen
+//Numeric Types and Operations
+// https://wiki.sei.cmu.edu/confluence/display/java/NUM03-J.+Use+integer+types+that+can+fully+represent+the+possible+range+of++unsigned+data
+
+import java.io.IOException;
+import java.io.*;
+
+//couldnt figure out how to get it to work because of needed input files
+public class R03_NUM03_J {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static long getInteger(DataInputStream is) throws IOException {
+  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
 }
