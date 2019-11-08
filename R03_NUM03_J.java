9f31839b0a5265745d79bad9c7ca3f243b7b3c01
Jonathon Irigoyen <Jonathon@joes-air.home>
2019-11-07 19:27:15 -0500

    Noncompliant Solution

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -14,7 +14,7 @@ public class R03_NUM03_J {
 
   }
 
-  public static long getInteger(DataInputStream is) throws IOException {
-  return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+  public static int getInteger(DataInputStream is) throws IOException {
+    return is.readInt();
 }
 }
